dec2a65182acd0f658432370fddb3ed9078aacb8
Maksym Nechaiev <Maksym_Nechaiev@epam.com>
2021-12-14 15:13:22 +0200

    new tests

---
+++ pkg/server/middleware/errorLogger_test.go
@@ -0,0 +1,9 @@
+package middleware
+
+import (
+	"testing"
+)
+
+func TestErrorLogger(t *testing.T) {
+
+}
---
+++ pkg/server/middleware/requestLogger_test.go
@@ -0,0 +1,9 @@
+package middleware
+
+import (
+	"testing"
+)
+
+func TestRequestLogger(t *testing.T) {
+
+}
