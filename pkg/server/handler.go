da6db029112354277a40c2e5a0fe8aae81990da4
Maksym Nechaiev <Maksym_Nechaiev@epam.com>
2022-02-02 12:26:52 +0200

    Task 09 Added pprof

--- pkg/server/handler.go
+++ pkg/server/handler.go
@@ -2,12 +2,12 @@ package serv
 
 import (
 	"github.com/MrKAKTyC/lets-go-chat/pkg/controller"
-	"log"
-	"net/http"
-
 	serv "github.com/MrKAKTyC/lets-go-chat/pkg/generated"
 	"github.com/MrKAKTyC/lets-go-chat/pkg/server/middleware"
+	echopprof "github.com/hiko1129/echo-pprof"
 	"github.com/labstack/echo/v4"
+	"log"
+	"net/http"
 )
 
 func Serve(port string, userController controller.User) {
@@ -16,6 +16,7 @@ func Serve(port string, userController controller.User) {
 	router.Use(middleware.PanicRecoverer)
 	router.Use(middleware.ErrorLogger)
 	router.Use(middleware.RequestLogger)
+	echopprof.Wrap(router)
 	serv.RegisterHandlers(router, &userController)
 
 	if err := http.ListenAndServe(":"+port, router); err != nil {
